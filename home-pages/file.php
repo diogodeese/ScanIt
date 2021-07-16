d8aa5e2fd2d902199b6bab7b12e4949b07ba3bf8
Diogo Santos <diogodeese@gmail.com>
2021-07-16 21:07:01 +0100

    16-07

--- home-pages/file.php
+++ home-pages/file.php
@@ -1,74 +1,163 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<title> ScanIt </title>
+		
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+<!-- ======================= CSS ======================== -->
+		<link rel="stylesheet" href="../css/page.css">		
+		<link rel="stylesheet" href="../css/util.css">		
+		<link rel="stylesheet" href="../css/table.css">
+		<link rel="stylesheet" href="../include/fontawesome/css/all.css">
+<!-- ==================================================== -->
+
+	</head>
+
+	<?php
 
     session_start();
     if(empty($_SESSION['username'])) { header('../index'); }
     require('../include/database-connection.php');
 
-?>
+	?>
 
-    <center>
-        <a href="home">Home</a>
-        <a href="../include/logout.php">Logout</a>
+	<body>
+	 
+		<div class="page-container" style="background-image: url('../images/main-wallpaper.jpg');" ><!-- PAGE - CONTEINER -->
 
-        <?php echo "<br><br><br><br>".$_SESSION['username']."<br><br><br>"; ?>
-    <center>
+			<div class="menu-container" ><!--RIGHT NAV-->
 
-<?php
+				<div class="menu-text p-r-30 p-l-30 p-t-50"><!-- MENU TEXT-->
 
-    require('include/file-creation.php');
+					<img src="/mail_html/images/code.jpg" width="35px" height="35px">SCANIT
 
-    $sql = "SELECT id, name, date_upload, unic_link FROM files WHERE id_users = $user[id] AND active = 1 AND unic_link = '$_GET[page]'";
-    $result = $db->query($sql);
+				</div><!-- MENU TEXT-->
 
-    if ($result->num_rows > 0) {
-        echo "<table border='1px'>
-            <tr>
-                <th> ID </th>
-                <th> Imagem </th>
-                <th> Data </th>
-                <th> Apagar </th>
-            </tr>
-        "; 
+				<div class="menu-bar p-t-60" ><!-- MENU BAR -->
 
-        $arrayCounter = 0;
-        while($row = $result->fetch_assoc()) {	
-            
-            $unic_link = $row['unic_link'];
-            $_SESSION['fileName'] = $row['name'];
+					<div class="menu-bar-a p-r-20"><!-- MENU BAR A --><!-- TRAOCAR HTML POR PHP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
 
-            echo "<tr>
-                    <td> ".$row["id"]."  </td>
-                    <td> <a href=home.php?page=".$row['unic_link']." onclick='generateQR($arrayCounter)'><img src=".$filePath.$row['name']." width='500' height='400'></a></td>
-                    <td> ".$row["date_upload"]." </td>
-                    <td> <a href=include/options.php?options=remove&id=".$row['id']."><button> Apagar </button></a> </td>
-                </tr>
-            ";
+						<a class="m-b-10" href="HOME.php">Home</a>
+						<hr style="width:70%;float:right">
+						<a class="m-b-10 m-t-10" href="Trash.php">Trash</a>
+						<hr style="width:70%;float:right">
+						<a class="m-b-10 m-t-10 active" href="ScanIt.php">ScanIt</a>
+						<hr style="width:70%;float:right">
+						<a class="m-t-10 m-t-10" href="About.php">About</a>
+						<hr style="width:70%;float:right">
+						<a class="m-t-5 red-txt" href="#exit">Log Out</a>
 
-            $arrayCounter++;
-            
-            echo "<a href='$filePath$row[name]' download>Download</a>";
+					</div><!-- MENU BAR A -->
 
-        }
-    } else {
-        echo "Sem items";
-    }
-  
-    $host= gethostname();
-    $ip = gethostbyname($host);
+				</div><!-- MENU BAR -->
 
-    $qrCodeUrl = "http://".$ip."/pap-30-05/home-pages/download.php?path=".$filePath.$_SESSION['fileName'];
+			</div><!--RIGHT NAV-->
 
-?>
+			<div class="content-container"><!-- CONTENT CONTAINER -->
+				
+				<div class="container-header"><!-- CONTAINER HEADER -->
 
-    <div id="qrcode"></div>
+					<div class="header-features"><!-- HEADER FEATURES -->   
 
-<script src="../js/qrcode.min.js"></script>
-<script>
+                        <span class="header-text">
+                            ScanIt
+                        </span>
 
-    window.onload = function generateQR() {
-        urlValue = window.location.assign = "<?php echo $qrCodeUrl; ?>";
-        var qrCode = new QRCode(document.getElementById('qrcode'));
-        qrCode.makeCode(urlValue);
-    }
+					</div><!-- HEADER FEATURES -->
 
-</script>
+					<div class="header-account-info"><!-- HEADER ACCOUNT INFO -->
+
+						<div class="account-name"><!-- ACCOUNT NAME -->
+
+							<i class="fas fa-user fa-lg m-r-10"></i>
+
+							<span>
+								<?php
+								echo $_SESSION['username'];
+								?>
+							</span>
+
+						</div><!-- ACCOUNT NAME -->
+
+						<div class="account-settings m-r-120"><!-- ACCOUNT SETTINGS --> 
+							
+							<span>
+								<i class="fas fa-cog fa-2x "></i>
+								<i class="fas fa-chevron-down m-t-7 p-l-5"></i>
+							</span>
+
+						</div><!-- ACCOUNT SETTINGS -->
+
+					</div><!-- HEADER ACCOUNT INFO -->
+							
+				</div><!-- CONTAINER HEADER -->
+
+				<div class="dynamic-container"><!-- DYNAMIC CONTAINER -->
+
+                    <?php
+
+						require('include/file-creation.php');
+
+						$sql = "SELECT id, name, date_upload, unic_link FROM files WHERE id_users = $user[id] AND active = 1 AND unic_link = '$_GET[page]'";
+						$result = $db->query($sql);
+
+						if ($result->num_rows > 0) {
+							echo "<table border='1px'>
+								<tr>
+									<th> Imagem </th>
+									<th> File Name </th>
+									<th> Data </th>
+									<th> Size </th>
+								</tr>
+							"; 
+
+							$arrayCounter = 0;
+							while($row = $result->fetch_assoc()) {	
+								
+								$unic_link = $row['unic_link'];
+								$_SESSION['fileName'] = $row['name'];
+
+								echo "<tr>
+										
+										<td class = 'image'> <a href=home.php?page=".$row['unic_link']." onclick='generateQR($arrayCounter)'><img src=".$filePath.$row['name']." width='500' height='400'></a></td>
+										
+										<td id = 'name'> ".$row['name']." </td>
+
+										<td id = 'date'> ".$row["date_upload"]." </td>
+
+										<td id = 'size'>  </td>
+                                            
+									</tr>
+								";
+
+								$arrayCounter++;
+								
+								echo "<a href='$filePath$row[name]' download>Download</a>";
+
+							}
+						} else {
+							echo "Sem items";
+						}
+					
+						$host= gethostname();
+						$ip = gethostbyname($host);
+
+						$qrCodeUrl = "http://".$ip."/pap-30-05/home-pages/download.php?path=".$filePath.$_SESSION['fileName'];
+
+					?>
+
+					<a href="home">
+						Go Back
+					</a>
+
+				</div><!-- DYNAMIC CONTAINER -->
+				
+			</div><!-- CONTENT CONTAINER -->
+
+		</div><!-- PAGE - CONTEINER -->
+		
+	</body>
+
+</html>
